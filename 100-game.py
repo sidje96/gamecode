36e876d50b47674965e866afd5b60d27f2a90a72
Thomas Broch <thomasb@q42.nl>
2023-07-23 12:54:30 +0200

    created main func

--- 100-game.py
+++ 100-game.py
@@ -9,35 +9,14 @@ CPU_NUM = 0
 CURRENT_SCORE = 0
 CURRENT_PLAYER = "Player 1"
 CPU_OPTIONS = [1, random.randint(1, 10), 10, 9, 8, 7, 6, 5, 4, 3, 2]
-
-print("""
-The 100-game is played as follows:
-You pick a number from 1-10. It can't be less than 1
-or bigger than 10
-You and a CPU or second player take turns.
-The number you or the opponent picks, gets
-added to the total score.
-Whoever gets to 100 first wins the game.""")
-
-
-player_amount = input(
-    """
-Do you wanna play against a CPU
-or against a friend?
-Type:
-0 for easy CPU
-1 for a more challenging experience against the CPU
-2 for 2 players
-3 for 3 players
-4 for perfect playing CPU\n""")
-
+PLAYER_AMOUNT = 0
 
 def cpu():
     global CURRENT_SCORE
-    if player_amount == "0":
+    if PLAYER_AMOUNT == "0":
         cpu_num = random.randint(1, 10)
         return cpu_num
-    elif player_amount == "1":
+    elif PLAYER_AMOUNT == "1":
         if CURRENT_SCORE % 11 == 1:
             cpu_num = CPU_OPTIONS[random.randint(1, 10)]
             return cpu_num
@@ -59,7 +38,7 @@ def player():
     global CURRENT_SCORE
 
     while player_input < 1 or player_input > 10:
-        if player_amount == "2":
+        if PLAYER_AMOUNT == "2":
             if CURRENT_PLAYER == "Player 1":
                 print("\nIt is", CURRENT_PLAYER, "'s turn")
                 player_input = input("Pick a number from 1-10 ")
@@ -93,7 +72,7 @@ def player():
                         CURRENT_PLAYER = "Player 1"
                         continue
 
-        elif player_amount == "3":
+        elif PLAYER_AMOUNT == "3":
             if CURRENT_PLAYER == "Player 1":
                 print("\nIt is", CURRENT_PLAYER, "'s turn")
                 player_input = input("Pick a number from 1-10 ")
@@ -158,7 +137,7 @@ def player():
                 return check_win()
 
 
-def check_win():
+def check_win(): #It's always good to make function names as descriptive as possible. "check_win" might make it a bit more clear what this function does, rather than "gamestate".
     global CURRENT_SCORE
     if CURRENT_SCORE >= 100:
         return True
@@ -186,16 +165,45 @@ def gamecpu():
         print(f"CPU won the game.")
 
 
-p_a_check = ["0", "1"]
+#I've moved a bunch of logic into the "main" function.
+# Earlier, there was some code running at the op of the file, then some functions were declared, and then it ran some more code.
+# This way, your code has a very clear "entry point", or starting point, from where you can easily follow what it's running.
+def main(): 
+    global PLAYER_AMOUNT
+    print("""
+    The 100-game is played as follows:
+    You pick a number from 1-10. It can't be less than 1
+    or bigger than 10
+    You and a CPU or second player take turns.
+    The number you or the opponent picks, gets
+    added to the total score.
+    Whoever gets to 100 first wins the game.""")
 
-if player_amount in p_a_check:
-    start = input("Do you want to start? ").lower()
-    if start == "yes":
-        player()
-    gamecpu()
-elif player_amount == "4":
-    gamecpu()
-else:
-    while not check_win():
-        player()
-    print(f"{CURRENT_PLAYER} won the game")
+
+    PLAYER_AMOUNT = input(
+        """
+    Do you wanna play against a CPU
+    or against a friend?
+    Type:
+    0 for easy CPU
+    1 for a more challenging experience against the CPU
+    2 for 2 players
+    3 for 3 players
+    4 for perfect playing CPU\n""")
+
+    
+    p_a_check = ["0", "1"]
+
+    if PLAYER_AMOUNT in p_a_check:
+        start = input("Do you want to start? ").lower()
+        if start == "yes":
+            player()
+        gamecpu()
+    elif PLAYER_AMOUNT == "4":
+        gamecpu()
+    else:
+        while not check_win():
+            player()
+        print(f"{CURRENT_PLAYER} won the game")
+
+main() #So this is where we actually start running code
